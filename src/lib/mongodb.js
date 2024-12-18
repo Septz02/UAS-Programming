8c2e625064fa1bd65f86520a0ced8582f7d9d806
Septz02 <dwiseptiadi0209@gmail.com>
2024-12-18 20:44:59 +0700

    a

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -11,13 +11,19 @@ const options = {
 let client;
 let clientPromise;
 
-// Ensure MongoDB URI is defined
-if (!uri) {
+if (!process.env.MONGODB_URI) {
   throw new Error("Add Mongo URI to .env.local");
 }
 
-// Create and connect the MongoClient directly in production
-client = new MongoClient(uri, options);
-clientPromise = client.connect();
+if (process.env.NODE_ENV === "development") {
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri, options);
+    global._mongoClientPromise = client.connect();
+  }
+  clientPromise = global._mongoClientPromise;
+} else {
+  client = new MongoClient(uri, options);
+  clientPromise = client.connect();
+}
 
 export default clientPromise;
